59c91d52151c3275c122c9c3823976dd907eccb3
Parker <psprouse@uno.edu>
2018-04-23 14:50:44 -0500

    Added DND page

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 // $("#submit").click(function() {
 //   var $btn = $(this);
-//   console.log($('#email'))
-//   console.log($('#message'))
 //   if ($('#email').length > 0 && $('#message').length > 0)
 //     $btn.button('loading');
 // });
