9f7b0f9d0fb944f4e06cfdc542d3a6ebd25f5211
Parker <psprouse@uno.edu>
2018-04-23 14:21:40 -0500

    Trying to fix this submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-$("#submit").click(function() {
-  var $btn = $(this);
-  if ($('#email').length > 0 && $('#message').length > 0)
-    $btn.button('loading');
-});
+// $("#submit").click(function() {
+//   var $btn = $(this);
+//   console.log($('#email'))
+//   console.log($('#message'))
+//   if ($('#email').length > 0 && $('#message').length > 0)
+//     $btn.button('loading');
+// });
