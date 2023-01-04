9a0b4ae30d73399ebf98e6762abc8e7ee705d72c
parkersprouse <parker@parkersprouse.me>
2023-01-04 20:24:00 +0000

    Deploying built app to Pages

--- index.792ef5da.js
+++ index.792ef5da.js
@@ -1,2 +1,2 @@
 document.addEventListener("DOMContentLoaded",(()=>{const e=document.querySelector(".title"),t=()=>{var t;window.innerWidth<768?e&&(void 0!==(t=e)?.style&&(t.style.fontSize=.75*t.offsetWidth+"%")):e.style.fontSize=null};window.addEventListener("resize",t),t()}));
-//# sourceMappingURL=index.60428e7a.js.map
+//# sourceMappingURL=index.792ef5da.js.map
