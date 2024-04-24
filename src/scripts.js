882ae09a357ab1660d2de1163463428a3d08aa1a
Parker Sprouse <parker@parkersprouse.me>
2024-04-23 23:25:56 -0500

    Switched to node 20; finally readded dark mode

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,8 @@
-/**
- * Set up a window resize listener that adjusts the page header's font size to fit the width of the
- * page if the header is too big.
- */
 document.addEventListener('DOMContentLoaded', () => {
+  /**
+   * Set up a window resize listener that adjusts the page header's font size to fit the width of the
+   * page if the header is too big.
+   */
   const title = document.querySelector('.title');
   if (title) {
     new ResizeObserver(() => {
@@ -15,66 +15,40 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }).observe(document.body);
   }
-});
 
-/*
- * See comment in HTML for explanation on why this is disabled.
- */
-
-/*
-const Themes = Object.freeze({
-  Dark: 'dark',
-  Light: 'light',
-});
-
-document.addEventListener('DOMContentLoaded', () => {
+  const Theme = Object.freeze({
+    Dark: 'dark',
+    Light: 'light',
+  });
   const toggle_container = document.querySelector('.theme-toggle');
   const toggle_link = toggle_container.querySelector('a');
   const body = document.querySelector('body');
 
   function toggleTheme(theme) {
-    if (theme === Themes.Dark) {
+    if (theme === Theme.Dark) {
       body.classList.remove('light');
       body.classList.add('dark');
-      toggle_link.innerText = 'go light';
+      toggle_link.innerText = 'light mode';
     } else {
       body.classList.remove('dark');
       body.classList.add('light');
-      toggle_link.innerText = 'go dark';
+      toggle_link.innerText = 'dark mode';
     }
     window.localStorage.setItem('theme', theme);
   }
 
-  toggle_link.onclick = (e) => {
+  toggle_link.addEventListener('click', (e) => {
     e.preventDefault();
-    if (body.classList.contains('light')) toggleTheme(Themes.Dark);
-    else toggleTheme(Themes.Light);
-  };
+    if (body.classList.contains('light')) toggleTheme(Theme.Dark);
+    else toggleTheme(Theme.Light);
+  });
 
   const saved_theme = window.localStorage.getItem('theme');
   if (saved_theme) toggleTheme(saved_theme);
   // Don't want themes being inadvertently swapped if the user's browser doesn't support
   // the `window.matchMedia` method, so explicitly check for each match.
-  else if (window.matchMedia('(prefers-color-scheme: dark)').matches) toggleTheme(Themes.Dark);
-  else if (window.matchMedia('(prefers-color-scheme: light)').matches) toggleTheme(Themes.Light);
+  else if (window.matchMedia('(prefers-color-scheme: dark)').matches) toggleTheme(Theme.Dark);
+  else if (window.matchMedia('(prefers-color-scheme: light)').matches) toggleTheme(Theme.Light);
 
   toggle_container.style.display = 'initial';
 });
-*/
-
-/*
-function getDefaultFontSize() {
-  const element = document.createElement('div');
-  element.style.width = '1rem';
-  element.style.display = 'none';
-  document.body.append(element);
-
-  const widthMatch = window.getComputedStyle(element).getPropertyValue('width').match(/\d+/);
-  element.remove();
-
-  if (!widthMatch || widthMatch.length < 1) return null;
-
-  const result = Number(widthMatch[0]);
-  return Number.isNaN(result) ? null : result;
-}
-*/
