7029e8a16a6b8739f55c876faf7e1e6e5edfb0ea
parkersprouse <parker@parkersprouse.me>
2024-04-24 04:26:23 +0000

    Deploying built app to Pages

--- scripts.f62261753aa7a671ff2a476783802ee93d04bc3574fafbc6b081b687f58709e4.js
+++
@@ -1,2 +0,0 @@
-(()=>{document.addEventListener("DOMContentLoaded",()=>{let e=document.querySelector(".title");e&&new ResizeObserver(()=>{window.innerWidth<768?e.style!==void 0&&(e.style.fontSize=`${e.offsetWidth*.75}%`):e.style.fontSize=void 0}).observe(document.body)});})();
-//# sourceMappingURL=scripts.f62261753aa7a671ff2a476783802ee93d04bc3574fafbc6b081b687f58709e4.js.map
