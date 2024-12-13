143dad29c56bed39ec70cd9c37f8f90ab6cfc4e6
Parker Sprouse <parker@parkersprouse.me>
2024-12-13 08:34:19 -0600

    Update build + deploy action

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   appType: 'custom',
+  base: '/',
   build: {
     assetsDir: '',
     copyPublicDir: true,
     cssMinify: 'lightningcss',
     emptyOutDir: true,
+    outDir: 'dist',
     sourcemap: true,
     target: 'es2018',
   },
