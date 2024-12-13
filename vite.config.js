ad0818f9bf45c19c134e1c55da89b06831751308
Parker Sprouse <parker@parkersprouse.me>
2024-12-13 08:41:52 -0600

    Attempt vite build fix

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   appType: 'custom',
-  base: '/',
   build: {
-    assetsDir: '',
+    assetsDir: '.',
     copyPublicDir: true,
     cssMinify: 'lightningcss',
     emptyOutDir: true,
-    outDir: 'dist',
     sourcemap: true,
     target: 'es2018',
   },
